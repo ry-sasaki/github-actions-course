a7e074554b7087a6bfe08ab036f5c57dc47b13b5
ry-sasaki <127266267+ry-sasaki@users.noreply.github.com>
2026-03-28 22:13:15 +0900

    feat(17-custom-actions): parse and validate inputs

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -58,13 +58,13 @@ async function run() {
     core.info('[js-dependency-update] : No updates at this point in time.');
   }
   /*
-  1. Parse inputs:
+  [DONE] 1. Parse inputs:
     1.1 base-branch from which to check for updates
     1.2 target-branch to use to create the PR
     1.3 Github Token for authentication purposes (to create PRs)
     1.4 Working directory for which to check for dependencies
-  2. Execute the npm update command within the working directory
-  3. Check whether there are modified package*.json files
+  [DONE] 2. Execute the npm update command within the working directory
+  [DONE] 3. Check whether there are modified package*.json files
   4. If there are modified files:
     4.1 Add and commit files to the target-branch
     4.2 Create a PR to the base-branch using the octokit API
